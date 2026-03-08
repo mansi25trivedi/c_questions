e2fc7a9940e1cb9a938fef684cc3a00e0ab579b2
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-08 15:05:03 +0000

    strings

---
+++ cp14.c
@@ -0,0 +1,8 @@
+//input two strings and print both the strings in diffrent lines
+#include<stdio.h>
+int main(){
+    char s1[20] , s2[20];
+    printf("enter two strings: ");
+    scanf("%s %s",s1 , s2);
+    printf("your first string is : %s and \n %s", s1 , s2);
+}
