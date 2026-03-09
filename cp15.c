80a71d15fa7d9f1221169545a2e10b19204d1d38
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-09 18:04:41 +0000

    not complete

---
+++ cp15.c
@@ -0,0 +1,11 @@
+//input the dd mm yyyy from the user andn input it in this format "dd/mm/yyyy"
+#include<stdio.h>
+int main(){
+    int d, m, y;
+    printf("enter any date(dd mm yyyy)");
+    if(scanf("%d %d %d",&d,&m,&y)== 3){
+        printf("")
+
+    }
+
+}
