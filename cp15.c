78774c3e4ec767e66474608eb20053445e9d5cb8
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-10 12:30:28 +0000

    input till eof

--- cp15.c
+++ cp15.c
@@ -4,8 +4,10 @@ int main(){
     int d, m, y;
     printf("enter any date(dd mm yyyy)");
     if(scanf("%d %d %d",&d,&m,&y)== 3){
-        printf("")
+        printf("the date is : %02d/%02d/%04d",d , m, y);
 
+    }else{
+        printf("invalid input : enter in the form of dd mm yyyy");
     }
 
 }
